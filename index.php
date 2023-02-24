fa3a98cc3023aebfeb21e6a802c8b933da70ec39
albert <aberttang@gmail.com>
2023-02-24 00:42:14 +0000

    update

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
  */
 define( 'WP_USE_THEMES', true );
 ob_start('ob_gzhandler');
-
+//echo "ok";
+//exit();
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
